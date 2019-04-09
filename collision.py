01be01d76d95204c2f0213af4035d1c9542f3ee3
graceborges <graceborges@mail.weber.edu>
2019-04-08 22:15:40 -0600

    Final collision edits

--- collision.py
+++ collision.py
@@ -18,10 +18,10 @@ def ball_collide(b1, b2):
 
     #formula for calculation: sqrt((x2-x1)^2 + (Y2-y1)^2 + (Z2-z1)^2)
     #R is added together for comparison
-    X = pow(int(x2) - int(x1),2)
-    Y = pow(int(y2) - int(y1),2)
-    Z = pow(int(z2) - int(z1),2)
-    R = (int(r2) + int(r1))
+    X = pow(float(x2) - float(x1),2)
+    Y = pow(float(y2) - float(y1),2)
+    Z = pow(float(z2) - float(z1),2)
+    R = (float(r2) + float(r1))
 
     distance = math.sqrt(X + Y + Z)
 
@@ -47,18 +47,29 @@ def main():
     b1 = ()
     b2 = ()
 
+    #for running as an independent program
+    f = open("/home/lrowe/submit/files/balltest.txt", "rt")
+    for line in f:
+        line = line.replace('\n','')
+        l = line.split(':')
+
+        b1 = tuple((l[0]).split(','))
+        b2 = tuple((l[1]).split(','))
+
+        print('b1: ', b1, '\tb2: ', b2)
+
     #Storing input into two tuples hopefully entered in correctly
-    b1 = tuple(input('Please enter coordinates for ball 1 in format - x,y,z,r: ').split(','))
-    b2 = tuple(input('Please enter coordinates for ball 2 in format - x,y,z,r: ').split(','))
+    #b1 = tuple(input('Please enter coordinates for ball 1 in format - x,y,z,r: ').split(','))
+    #b2 = tuple(input('Please enter coordinates for ball 2 in format - x,y,z,r: ').split(','))
     
     #handling incorrect input with value error
-    try:
-        ball_collide(b1,b2)
+        try:
+            ball_collide(b1,b2)
 
-    except ValueError:
-        print('Please try again and  enter coordinates correctly')
-        return -1
+        except ValueError:
+            print('Please try again and  enter coordinates correctly')
+            return -1
+
 
 if __name__ == '__main__':
     main()
-
